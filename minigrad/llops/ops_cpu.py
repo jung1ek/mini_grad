3ca1e44527c8e03c2bc78bc949282d83b438464d
Tek Jung <tekjungsingh659@gmail.com>
2025-12-21 22:27:44 +0545

    shrink (slice) implementation

--- minigrad/llops/ops_cpu.py
+++ minigrad/llops/ops_cpu.py
@@ -5,6 +5,7 @@ from minigrad.ops import BinaryOps,UnaryOps,MovementOps,ReduceOps,ProcessingOps
 import operator
 from minigrad.helpers import shape_to_axis,ConvArgs
 
+# ndarray for data type and cpu operations; GenericExecAST for tree execution
 class CPUBuffer(np.ndarray,GenericExecAST):
     # all the x are realized data.
     fxn_for_op = {
@@ -12,7 +13,9 @@ class CPUBuffer(np.ndarray,GenericExecAST):
         MovementOps.EXPAND: lambda x,shape: CPUBuffer.expand(x,shape), MovementOps.RESHAPE: lambda x, shape: CPUBuffer.reshape(x,shape),
         MovementOps.PERMUTE: lambda x, order: CPUBuffer.permute(x,order),
         ReduceOps.SUM: lambda x, axis,keepdims: x.sum(axis=axis,keepdims=keepdims).view(CPUBuffer), ReduceOps.MAX: None,
-        MovementOps.STRIDED: lambda x,args: CPUBuffer.strided(x,args)
+        MovementOps.STRIDED: lambda x,args: CPUBuffer.strided(x,args),
+        MovementOps.SHRINK: lambda x, arg: x[tuple(slice(p[0],p[1],None) for p in arg)],
+        MovementOps.PAD: lambda x, padding: CPUBuffer.pad(x,padding)
     }
 
     def relu(x): return np.maximum(0,x)
@@ -35,7 +38,7 @@ class CPUBuffer(np.ndarray,GenericExecAST):
     def load_op(x): return CPUBuffer.fromCPU(x)
     def unary_op(x,op): return CPUBuffer.fxn_for_op[op](x)
     def binary_op(x,op,y): return CPUBuffer.fxn_for_op[op](x,y)
-    def movement_op(x,op,shape_order): return CPUBuffer.fxn_for_op[op](x,shape_order)
+    def movement_op(x,op,arg): return CPUBuffer.fxn_for_op[op](x,arg)
     def reduce_op(x,op,axis,keepdims): return CPUBuffer.fxn_for_op[op](x,axis,keepdims)
     
     def processing_op(x,op,w,C: ConvArgs):
