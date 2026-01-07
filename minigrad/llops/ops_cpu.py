4d35384ee0ab1bd297e396e99175f642b53c16fc
Tek Jung <tekjungsingh659@gmail.com>
2026-01-07 21:12:25 +0545

    added function_to_op for unary operations

--- minigrad/llops/ops_cpu.py
+++ minigrad/llops/ops_cpu.py
@@ -8,16 +8,25 @@ from minigrad.helpers import shape_to_axis,ConvArgs
 # ndarray for data type and cpu operations; GenericExecAST for tree execution
 class CPUBuffer(np.ndarray,GenericExecAST):
     # all the x are realized data.
+    # class variable
     fxn_for_op = {
-        BinaryOps.MUL: operator.mul, BinaryOps.ADD: operator.add,
-        MovementOps.EXPAND: lambda x,shape: CPUBuffer.expand(x,shape), MovementOps.RESHAPE: lambda x, shape: CPUBuffer.reshape(x,shape),
+        BinaryOps.MUL: operator.mul, BinaryOps.ADD: operator.add, UnaryOps.RECIPROCAL: lambda x: 1.0/x,
+        BinaryOps.POW: operator.pow, BinaryOps.SUB: operator.sub, BinaryOps.DIV: operator.truediv,
+        UnaryOps.NEG: lambda x: -x, UnaryOps.SIGN: lambda x: x.sign(), UnaryOps.EXP: lambda x: x.exp(),
+        UnaryOps.RELU: lambda x: x.relu(),UnaryOps.LOG: lambda x: x.log(),
+        MovementOps.EXPAND: lambda x,shape: CPUBuffer.expand(x,shape),
+        MovementOps.RESHAPE: lambda x, shape: CPUBuffer.reshape(x,shape),
         MovementOps.PERMUTE: lambda x, order: CPUBuffer.permute(x,order),
-        ReduceOps.SUM: lambda x, axis,keepdims: x.sum(axis=axis,keepdims=keepdims).view(CPUBuffer), ReduceOps.MAX: None,
+        ReduceOps.SUM: lambda x, axis,keepdim: x.sum(axis=axis,keepdims=keepdim).view(CPUBuffer),
+        ReduceOps.MAX: None,
         MovementOps.STRIDED: lambda x,args: CPUBuffer.strided(x,args),
         MovementOps.SHRINK: lambda x, arg: x[tuple(slice(p[0],p[1],None) for p in arg)],
-        MovementOps.PAD: lambda x, padding: CPUBuffer.pad(x,padding)
+        MovementOps.PAD: lambda x, padding: CPUBuffer.pad(x,padding),
+        MovementOps.MASKED_FILL: lambda x,args: x.masked_fill(*args)
     }
 
+    # object methods
+    def masked_fill(x,mask,value): x[mask]=value; return x
     def relu(x): return np.maximum(0,x)
     def log(x): return np.log(x)
     def exp(x): return np.exp(x)
@@ -39,7 +48,7 @@ class CPUBuffer(np.ndarray,GenericExecAST):
     def unary_op(x,op): return CPUBuffer.fxn_for_op[op](x)
     def binary_op(x,op,y): return CPUBuffer.fxn_for_op[op](x,y)
     def movement_op(x,op,arg): return CPUBuffer.fxn_for_op[op](x,arg)
-    def reduce_op(x,op,axis,keepdims): return CPUBuffer.fxn_for_op[op](x,axis,keepdims)
+    def reduce_op(x,op,axis,keepdim): return CPUBuffer.fxn_for_op[op](x,axis,keepdim)
     
     def processing_op(x,op,w,C: ConvArgs):
         # TODO can use sliding window view? much safer
