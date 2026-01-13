399aa789fade8b44fee2d2b71536c49475fa8649
Tek Jung <tekjungsingh659@gmail.com>
2026-01-13 13:55:27 +0545

    Fix(recursion exceeded)

--- minigrad/ops.py
+++ minigrad/ops.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 from enum import Enum
 from typing import NamedTuple,Tuple,Union,Any,Type
+import sys
 
 #TODO load op, unary ops, binary ops, movement ops, reduce ops, 
-BinaryOps = Enum("BinaryOps",["MUL","ADD"])
-UnaryOps = Enum("UnaryOps",["RELU","EXP"])
+BinaryOps = Enum("BinaryOps",["MUL","ADD","SUB","POW","DIV","CMPEQ"])
+UnaryOps = Enum("UnaryOps",["RELU","EXP","RECIPROCAL","LOG","NEG","SIGN"])
 LoadOps = Enum("LoadOps",["FROMCPU"])
-MovementOps = Enum("MovementOps",["EXPAND","RESHAPE","PERMUTE","STRIDED","PAD","SHRINK"])
+MovementOps = Enum("MovementOps",["EXPAND","RESHAPE","PERMUTE","STRIDED","PAD","SHRINK","MASKED_FILL"])
 ReduceOps = Enum("ReduceOps",["SUM","MAX"])
 ProcessingOps = Enum("ProcessingOps",["CONV1D","CONV"])
+
+sys.setrecursionlimit(1000)
 
 Op = Union[LoadOps,UnaryOps,BinaryOps]
 OpType = Union[Type[BinaryOps],Type[UnaryOps],Type[LoadOps]]
@@ -22,15 +25,29 @@ class LazyOp(NamedTuple):
 #TODO visualiztion of tree execution
 class GenericExecAST:
     @classmethod
-    def exec_ast(cls: GeneratorExit, ast: LazyOp):
+    def exec_ast(cls: GenericExecAST, ast: LazyOp, preprocess= lambda x: x):
         # recursively execute the tree from srcs to the final node.
         # rel_srcs = realized sources
-        rel_srcs = [cls.exec_ast(x) if isinstance(x,LazyOp) else cls.exec_ast(x.op) for x in ast.src]
+        # rel_srcs = [cls.exec_ast(x) if isinstance(x,LazyOp) else x.realize() for x in ast.src]
+        #TODO minimize the recursion using below statement, explict call of exe_ast on lazybuffer
+        # rel_srcs = [cls.exec_ast(x,preprocess) if isinstance(x,LazyOp) else preprocess(x) for x in ast.src]
+
+        # If we got a LazyBuffer, realize it
+        if hasattr(ast, "realize"):
+            return ast.realize()
+
+        # Otherwise must be LazyOp
+        assert isinstance(ast, LazyOp)
+
+        rel_srcs = [cls.exec_ast(x) for x in ast.src]
+
         if ast.op in LoadOps:
             ret = cls.fromCPU(ast.arg)
         elif ast.op in UnaryOps:
+            # TODO shape assert
             ret = cls.unary_op(rel_srcs[0],ast.op)
         elif ast.op in BinaryOps:
+            assert rel_srcs[0].shape==rel_srcs[1].shape
             ret = cls.binary_op(rel_srcs[0], ast.op, rel_srcs[1])
         elif ast.op in ReduceOps:
             ret = cls.reduce_op(rel_srcs[0],ast.op,*ast.arg)
@@ -38,4 +55,6 @@ class GenericExecAST:
             ret = cls.movement_op(rel_srcs[0],ast.op,ast.arg)
         elif ast.op in ProcessingOps:
             ret = cls.processing_op(rel_srcs[0],ast.op,rel_srcs[1],ast.arg)
+        else:
+            raise Exception("Unknown Op")
         return ret
