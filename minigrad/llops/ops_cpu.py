c6b75c02665ad1d8e86fa7defbd8c3237771d83c
Tek Jung <tekjungsingh659@gmail.com>
2026-01-13 13:56:28 +0545

    added as arrary np to fix CPUBuffer class recurison loop.

--- minigrad/llops/ops_cpu.py
+++ minigrad/llops/ops_cpu.py
@@ -10,15 +10,15 @@ class CPUBuffer(np.ndarray,GenericExecAST):
     # all the x are realized data.
     # class variable
     fxn_for_op = {
-        BinaryOps.MUL: operator.mul, BinaryOps.ADD: operator.add, UnaryOps.RECIPROCAL: lambda x: 1.0/x,
-        BinaryOps.POW: operator.pow, BinaryOps.SUB: operator.sub, BinaryOps.DIV: operator.truediv,
+        BinaryOps.MUL: operator.mul, BinaryOps.ADD: operator.add, UnaryOps.RECIPROCAL: lambda x: (1.0/x).view(CPUBuffer),
+        BinaryOps.POW: operator.pow, BinaryOps.SUB: operator.sub, BinaryOps.DIV: operator.truediv, BinaryOps.CMPEQ: lambda x,y: (x==y).float().view(CPUBuffer),
         UnaryOps.NEG: lambda x: -x, UnaryOps.SIGN: lambda x: x.sign(), UnaryOps.EXP: lambda x: x.exp(),
         UnaryOps.RELU: lambda x: x.relu(),UnaryOps.LOG: lambda x: x.log(),
-        MovementOps.EXPAND: lambda x,shape: CPUBuffer.expand(x,shape),
-        MovementOps.RESHAPE: lambda x, shape: CPUBuffer.reshape(x,shape),
+        MovementOps.EXPAND: lambda x,shape: x.expand(shape),
+        MovementOps.RESHAPE: lambda x, shape: x.reshape(shape).view(CPUBuffer),
         MovementOps.PERMUTE: lambda x, order: CPUBuffer.permute(x,order),
         ReduceOps.SUM: lambda x, axis,keepdim: x.sum(axis=axis,keepdims=keepdim).view(CPUBuffer),
-        ReduceOps.MAX: None,
+        ReduceOps.MAX: lambda x, axis,keepdim: x.amax(axis=axis,keepdims=keepdim).view(CPUBuffer),
         MovementOps.STRIDED: lambda x,args: CPUBuffer.strided(x,args),
         MovementOps.SHRINK: lambda x, arg: x[tuple(slice(p[0],p[1],None) for p in arg)],
         MovementOps.PAD: lambda x, padding: CPUBuffer.pad(x,padding),
@@ -26,19 +26,19 @@ class CPUBuffer(np.ndarray,GenericExecAST):
     }
 
     # object methods
-    def masked_fill(x,mask,value): x[mask]=value; return x
+    def masked_fill(x,mask,value): x[mask]=value; return x.view(CPUBuffer)
     def relu(x): return np.maximum(0,x)
     def log(x): return np.log(x)
     def exp(x): return np.exp(x)
     def sign(x): return np.sign(x)
-    def float(x): return x.astype(np.float32)
-    def flip(x,axis): return np.flip(x,axis)
-    def amax(x,*args,**kwargs): return np.amax(x,*args,**kwargs)
-    def pad(x,padding): return np.pad(x,padding).view(CPUBuffer)
-    def expand(x,shape) : return np.broadcast_to(x,shape=shape).view(CPUBuffer)
-    def reshape(x,shape) : return np.reshape(x,shape).view(CPUBuffer)
-    def strided(x,arg): return np.lib.stride_tricks.as_strided(x.ravel().reshape(x.shape),shape=[y[0] for y in arg],strides=[y[1]*x.dtype.itemsize for y in arg]).view(CPUBuffer)
-    def permute(x,order) : return np.transpose(x,order).view(CPUBuffer)
+    def float(x): return x.astype(np.float32).view(CPUBuffer)
+    def flip(x,axis): return np.flip(x,axis).view(CPUBuffer)
+    def amax(x,*args,**kwargs): return np.amax(x,*args,**kwargs).view(CPUBuffer)
+    def pad(x,padding): return np.pad(np.asarray(x),padding).view(CPUBuffer)
+    def expand(x,shape) : return np.broadcast_to(np.asarray(x),shape=shape).view(CPUBuffer)
+    # def reshape(x,shape) : return np.reshape(x,shape).view(CPUBuffer)
+    def strided(x,arg): return np.lib.stride_tricks.as_strided(np.asarray(x).ravel().reshape(x.shape),shape=[y[0] for y in arg],strides=[y[1]*x.dtype.itemsize for y in arg]).view(CPUBuffer)
+    def permute(x,order) : return np.transpose(np.asarray(x),order).view(CPUBuffer)
 
     @staticmethod
     def fromCPU(x: np.ndarray) : return x.view(CPUBuffer)
