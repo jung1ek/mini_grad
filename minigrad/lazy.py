2cad136d506a0cc5e7799ee5639bc170cb4783e7
Tek Jung <tekjungsingh659@gmail.com>
2026-01-13 13:57:24 +0545

    match the realized shape and buffer shape

--- minigrad/lazy.py
+++ minigrad/lazy.py
@@ -8,24 +8,28 @@ from minigrad.helpers import reduce_shape
 from minigrad.helpers import ConvArgs, get_conv_args
 import sys
 
-sys.setrecursionlimit(1000)
+sys.setrecursionlimit(10000)
 
 class Device:
     pass
 
 # TODO two ops in a row is one op. merge them if unresolved, movement ops
+# TODO optimization, 
 class LazyBuffer:
     def __init__(self,shape,device,op_type:OpType,op:LazyOp):
         self.op_type = op_type
         self.shape = shape
         self.op: LazyOp = op
         self.device = device
-        self.realized = None # DeviceBuffer, computed data
+        self.realized = None # DeviceBuffer; computed data
         self.children = None # Other lazy Buffer
 
     def __repr__(self): return f"<LB {self.shape} op:{self.op.op if self.realized is None else 'realized'}>"
     
     def _realize_binaryops(self):
+        return CPUBuffer.exec_ast(self.op)
+    
+    def _realize_unaryops(self):
         return CPUBuffer.exec_ast(self.op)
 
     def _realize_loadops(self):
@@ -46,8 +50,11 @@ class LazyBuffer:
             return self.realized
         _realize = {LoadOps: self._realize_loadops,BinaryOps: self._realize_binaryops,
                     ReduceOps: self._realize_reduceops, MovementOps: self._realize_movementops,
-                    ProcessingOps: self._realize_processingops}
+                    ProcessingOps: self._realize_processingops, UnaryOps: self._realize_unaryops}
         self.realized = _realize[self.op_type]()
+        #TODO for padding shape calc
+        # assert self.realized.shape == self.shape,f"Shape mismatch: expected {self.shape}, got {self.realized.shape}"
+        
         del self.op
         return self.realized
 
@@ -62,9 +69,12 @@ class LazyBuffer:
     
     # creating lazy buffer through operations, z(new_buffer) = x(current_buffer)+(op) y(other_buffer)
     def movement_op(self,op:MovementOps,arg:tuple):
+        #TODO for pad and shrink
         if op is MovementOps.PERMUTE:
             # parameter shape is order for permute op.
             shape = tuple([self.shape[dim_idx] for dim_idx in arg])
+        elif op is MovementOps.MASKED_FILL:
+            shape = self.shape
         else:
             shape = arg
         return LazyBuffer(shape=shape,device=self.device,op_type=MovementOps,op=LazyOp(op,(self,),arg=arg))
@@ -75,8 +85,8 @@ class LazyBuffer:
     def unary_op(self,op:UnaryOps):
         return LazyBuffer(self.shape,self.device,UnaryOps,LazyOp(op,(self,)))
     
-    def reduce_op(self, op: ReduceOps,axis,keepdims):
-        return LazyBuffer(reduce_shape(self.shape,axis=axis,keepdims=keepdims),self.device,ReduceOps,LazyOp(op,(self,),arg=(axis,keepdims)))
+    def reduce_op(self, op: ReduceOps,axis,keepdim):
+        return LazyBuffer(reduce_shape(self.shape,axis=axis,keepdim=keepdim),self.device,ReduceOps,LazyOp(op,(self,),arg=(axis,keepdim)))
     
     def processing_op(x,op:ProcessingOps,w:LazyBuffer,arg: ConvArgs):
         # TODO, implement conv2d using expand,reshape,mul,sum
