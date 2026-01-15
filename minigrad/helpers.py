9a5f6ce985b687fd259a3b7f3c0141494c32f12b
Tek Jung <tekjungsingh659@gmail.com>
2026-01-15 16:21:48 +0545

    Fix(shape_to_axis)

--- minigrad/helpers.py
+++ minigrad/helpers.py
@@ -8,21 +8,9 @@ def normalize_axis(axis, ndim):
     return tuple(a if a >= 0 else a + ndim for a in axis)
 
 # axis based on the old shape and new shape
-# TODO issue, while (3,5,1) - > (3,5,6) , gives ()
-def shape_to_axis(old_shape,new_shape):
-    # Case 1: keepdims=True
-    if len(old_shape) == len(new_shape):
-        return tuple(i for i, (o, n) in enumerate(zip(old_shape, new_shape)) if n == 1 and o != 1)
-
-    # Case 2: keepdims=False
-    axes = []
-    j = 0  # pointer in new_shape
-    for i, o in enumerate(old_shape):
-        if j < len(new_shape) and new_shape[j] == o:
-            j += 1
-        else:
-            axes.append(i)
-    return tuple(axes)
+def shape_to_axis(old_shape, new_shape):
+  assert len(old_shape) == len(new_shape), "reduce shapes must have same dimensions"
+  return tuple([i for i,(a,b) in enumerate(zip(old_shape, new_shape)) if a != b])
 
 # new shape based on the axis to sum
 def reduce_shape(shape, axis,keepdim=False):
@@ -38,21 +26,13 @@ def reduce_shape(shape, axis,keepdim=False):
     
     if keepdim:
         # change reduce axis to 1
-        new_shape = [
-            1 if i in axes else shape[i]
-            for i in range(ndim)
-        ]
+        new_shape = [1 if i in axes else shape[i] for i in range(ndim)]
     else:
         # drop the reduced axis
-        new_shape = [
-            shape[i] for i in range(ndim) if i not in axes
-        ]
+        new_shape = [shape[i] for i in range(ndim) if i not in axes]
     return tuple(new_shape)
 
 # def reduce_shape(shape, axis): return tuple(1 if i in axis else shape[i] for i in range(len(shape)))
-def shape_to_axis(old_shape, new_shape):
-  assert len(old_shape) == len(new_shape), "reduce shapes must have same dimensions"
-  return tuple([i for i,(a,b) in enumerate(zip(old_shape, new_shape)) if a != b])
 
 ConvArgs = namedtuple("ConvArgs",['H', 'W', 'groups', 'rcout', 'cin', 'oy', 'ox', 'iy', 'ix', 'sy', 'sx', 'bs', 'cout', 'py', 'py_', 'px', 'px_', 'dy', 'dx', 'out_shape'])
 def get_conv_args(x_shape, w_shape, stride=1, groups=1, padding=0, dilation=1, out_shape=None):
